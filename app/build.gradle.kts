a2e57801d28dd42974ab310fb25971d48da24244
kaan8588 <kaan8588@users.noreply.github.com>
2026-05-14 23:39:33 +0300

    Update version to 1.0.3 and generate app bundle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.islistudios.muavin"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
